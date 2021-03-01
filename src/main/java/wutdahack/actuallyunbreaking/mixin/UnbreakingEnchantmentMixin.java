e453ee109b570ebf9febab4ee28acdb94bd97bbc
WUT DA HACK <70644502+WUTDAHACK@users.noreply.github.com>
2021-03-01 13:27:59 +0000

    trying to remove that annoying 1 durability being removed then added back on again

--- src/main/java/wutdahack/actuallyunbreaking/mixin/UnbreakingEnchantmentMixin.java
+++ src/main/java/wutdahack/actuallyunbreaking/mixin/UnbreakingEnchantmentMixin.java
@@ -4,21 +4,20 @@ import net.minecraft.enchantment.UnbreakingEnchantment;
 import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.Random;
 
 @Mixin(UnbreakingEnchantment.class)
 public class UnbreakingEnchantmentMixin {
 
-    @Inject(method = "shouldPreventDamage", at = @At(value = "INVOKE"))
-    private static void preventDamage(ItemStack item, int level, Random random, CallbackInfoReturnable<Boolean> cir) {
-        cir.isCancelled(); // i can't remember if this mixin worked without this line so i'm keeping it because it seems to work with it
-         if (item.isDamaged() || item.isDamageable()) {
-             item.setDamage(-2147483648);
-         }
+    // when preventing damage, sets the tool to be actually unbreakable
+    @Overwrite
+    // thanks to overwrite now it doesn't take 1 durability away and then add it back on - overwrite should be avoided but it worked better than inject??
+    public static boolean shouldPreventDamage(ItemStack item, int level, Random random) {
+        if (item.isDamageable() || item.isDamaged()) {
+            item.setDamage(-2147483648);
+        }
+        return true;
     }
-
+    
 }
