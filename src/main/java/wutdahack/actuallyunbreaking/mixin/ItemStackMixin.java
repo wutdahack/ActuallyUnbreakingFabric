4447562ecd1a65223180b9d99b35bde70a2a0b56
auoeke <username11681@gmail.com>
2021-08-05 10:24:41 +0100

    remove unnecessary opcode specifier

--- src/main/java/wutdahack/actuallyunbreaking/mixin/ItemStackMixin.java
+++ src/main/java/wutdahack/actuallyunbreaking/mixin/ItemStackMixin.java
@@ -12,7 +12,7 @@ import wutdahack.actuallyunbreaking.ActuallyUnbreaking;
 @Mixin(ItemStack.class)
 abstract class ItemStackMixin {
     @ModifyVariable(method = "damage(ILjava/util/Random;Lnet/minecraft/server/network/ServerPlayerEntity;)Z",
-                    at = @At(value = "STORE", ordinal = 0, opcode = Opcodes.ISTORE), ordinal = 1) // this injects after `i = EnchantmentHelper.getLevel(Enchantments.UNBREAKING, this);`
+                    at = @At(value = "STORE", ordinal = 0), ordinal = 1) // this injects after `i = EnchantmentHelper.getLevel(Enchantments.UNBREAKING, this);`
     public int repairUnbreakableItem(int level, int amount, Random random, ServerPlayerEntity player) {
         if (ActuallyUnbreaking.isUnbreakable(level)) {
             ((ItemStack) (Object) this).setDamage(0);
