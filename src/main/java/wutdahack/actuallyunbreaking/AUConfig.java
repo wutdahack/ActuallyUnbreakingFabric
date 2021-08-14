6d28fa9056241692a8c1ea92689deab0d68848c4
wutdahack <70644502+wutdahack@users.noreply.github.com>
2021-08-14 12:10:50 +0100

    made mending incompatibility optional

--- src/main/java/wutdahack/actuallyunbreaking/AUConfig.java
+++ src/main/java/wutdahack/actuallyunbreaking/AUConfig.java
@@ -16,4 +16,8 @@ public class AUConfig implements ConfigData {
     @Comment("only the last level of unbreaking will set the tool\nto be unbreakable if this is true. default = false")
     public boolean maxLevelOnly = false;
 
+    @ConfigEntry.Gui.RequiresRestart(value = false)
+    @Comment("unbreaking will be incompatible with mending\nif this is true. default = true")
+    public boolean mendingIncompatibility = true;
+
 }
