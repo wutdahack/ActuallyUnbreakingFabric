3507c3617e590b48d9cd55b6571f64ec8bf30a05
wutdahack <70644502+wutdahack@users.noreply.github.com>
2023-01-19 11:43:47 +0000

    fixed crash #9

--- src/main/java/wutdahack/actuallyunbreaking/mixin/EnchantRandomlyLootFunctionMixin.java
+++ src/main/java/wutdahack/actuallyunbreaking/mixin/EnchantRandomlyLootFunctionMixin.java
@@ -5,7 +5,7 @@ import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.enchantment.Enchantments;
 import net.minecraft.item.ItemStack;
 import net.minecraft.loot.function.EnchantRandomlyLootFunction;
-import net.minecraft.util.registry.Registry;
+import net.minecraft.registry.Registries;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
@@ -20,9 +20,9 @@ public abstract class EnchantRandomlyLootFunctionMixin {
     private boolean isBook;
     private ItemStack lootStack;
 
-    @ModifyVariable(method = "process", ordinal = 0, at = @At(value = "STORE"))
+    @ModifyVariable(method = "process", index = 6, at = @At(value = "STORE"))
     private List<Enchantment> filterMending(List<Enchantment> list) {
-         list = Registry.ENCHANTMENT.stream().filter(Enchantment::isAvailableForRandomSelection).filter(enchantment -> isBook || enchantment.isAcceptableItem(lootStack)).filter(
+         list = Registries.ENCHANTMENT.stream().filter(Enchantment::isAvailableForRandomSelection).filter(enchantment -> isBook || enchantment.isAcceptableItem(lootStack)).filter(
                     enchantment -> {
                         if (ActuallyUnbreaking.instance.config.editEnchantedLootGeneration) {
                             if (EnchantmentHelper.getLevel(Enchantments.UNBREAKING, lootStack) > 0) {
